1b002534921f7975c26f18eb5bff35cb590060b2
release <0183535663@naver.com>
2017-01-25 18:33:45 +0900

    Added main_page and MainActivity

---
+++ app/src/main/java/com/release/release/MainActivity.java
@@ -0,0 +1,10 @@
+package com.release.release;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by release on 2017. 1. 25..
+ */
+
+public class MainActivity extends AppCompatActivity {
+}
